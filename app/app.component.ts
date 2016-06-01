16a82cdb794cfcabd9da5624ade356174cce7e9d
Andrew Russo and Daniel Wulff <daniel@>
2016-06-01 09:45:51 -0700

    working placeholder text

---
+++ app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component, EventEmitter } from 'angular2/core';
+// import { TaskListComponent } from './task-list.component';
+// import { Task } from './task.model';
+
+@Component({
+  selector: 'my-app',
+  template: `
+    <h1>Tap List</h1>
+  `
+})
+
+export class AppComponent {
+
+
+}
